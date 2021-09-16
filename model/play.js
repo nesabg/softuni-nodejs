e66d0b85cdc11b3f952fd8b2ff736f1cc513804e
Asen Asenov <nesabg2@gmail.com>
2021-09-16 14:31:10 +0300

    Adding plays model

---
+++ model/play.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const PlaySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 50,
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    isPublic: {
+        type: Boolean
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    },
+    UsersLiked: {
+        type: Array
+    }
+})
+
+module.exports = mongoose.model('Play', PlaySchema)
