4f07079b240f3c63ced2d349de7071c69df308e0
Asen Asenov <53828105+nesabg@users.noreply.github.com>
2021-10-05 23:35:16 +0300

    fix index js

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ mongoose.connect(process.env.DB_URI, {
         })
     })
 
-    app.listen(cprocess.env.port, console.log(`Database and Server is up and running on port ${process.env.port}`))
+    app.listen(process.env.port, console.log(`Database and Server is up and running on port ${process.env.port}`))
   }) 
 
